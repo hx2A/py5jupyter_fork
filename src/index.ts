d04bdd4373b20d330d5a816bea186a34ea761294
Jim <james.schmitz@gmail.com>
2023-12-04 09:16:13 -0500

    take 2

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
-// *****************************************************************************
-//
-//   Part of the py5jupyter (& py5) library
-//   Copyright (C) 2022-2023 Jim Schmitz
-//
-//   This library is free software: you can redistribute it and/or modify it
-//   under the terms of the GNU Lesser General Public License as published by
-//   the Free Software Foundation, either version 2.1 of the License, or (at
-//   your option) any later version.
-//
-//   This library is distributed in the hope that it will be useful, but
-//   WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
-//   General Public License for more details.
-//
-//   You should have received a copy of the GNU Lesser General Public License
-//   along with this library. If not, see <https://www.gnu.org/licenses/>.
-//
-// *****************************************************************************
-export * from './version';
-export * from './widget';
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+/**
+ * Initialization data for the jupyter-py5 extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyter-py5:plugin',
+  description: 'py5 Jupyter tools',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyter-py5 is activated!');
+  }
+};
+
+export default plugin;
